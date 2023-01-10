67a05b201f5b8859abae9275581536a54c029ed7
Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>
2023-01-09 19:12:47 -0700

    first commit

---
+++ capturar letras y devolver vocal/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//27.- Que el usuario ingrese 6 letras, y
+//cada vez que ingresa una vocal, imprime "VOCAL" y la letra.
+//Datos de entrada
+string[] letra = new string[4];
+//Procedimiento
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("Captura tu letra " + i+1);
+}
